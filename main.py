efda27aa12d7118e4cc4a32c508d3b832bbafd65
GhastCMD <kraken32bin@protonmail.com>
2021-06-01 20:54:51 -0300

    Corrected a bug with range part with comma separation without spaces

--- main.py
+++ main.py
@@ -1,36 +1,3 @@
-'''
-Basicamente se utilizam duas bibliotecas para desenvolver
-o projeto: a biblioteca panda para poder ler e interpretar
-o arquivo da base de dados csv, e a biblioteca sklearn, que
-é responsável por criar o modelo da árvore de decisões.
-
-Primeiramente se abre o arquivo da base de dados com o módulo
-panda que será responsvável pela vizualização da base de dados,
-depois se definirá qual o vetor de alvos que será a reposta que
-pretendemos alcançar.
-
-O comando 'drop' serve para tirar determinados valores do arquivo
-csv, e o valor "axis='column'" significa que irá remover todos os
-valores do eixo vertical do arquivo. Os dados são acessados por
-um hashmap que toma como parâmetro o valor do nome da coluna.
-
-Depois de separado os valores paramétricos dos valores de alvo,
-se realiza a codificação dos parâmetros, que transforma os valores
-em texto para valores numéricos que são mais fáceis de serem
-trabalhados pelo programa. Para isso, utiliza-se do submódulo
-preprocessing da biblioteca sklearn, com a função LabelEncoder.
-
-LabelEncoder().fit_transform(<name of column to encode>)
-
-Então, com os dados codificados, instancia-se uma árvore de 
-classificação por decisão e coloca-se os valores que obtivermos
-até então como primeiro argumento, e como segundo argumento, o
-vetor de alvo que se obteve no início.
-
-Para verificar algum alvo arbitrário, basta chamar a função
-predict da árvore e colocar os valores arbitrários como parâmetros.
-'''
-
 from unidecode import unidecode
 
 def comp_str(string: str):
@@ -128,7 +95,7 @@ def parse_file(file_path: str) -> Disturbance:
                 values = value_tokens
             elif line[0] == '[':
                 range_tokens = line.strip('[ ').split('] ')
-                rng_vals = (range_tokens[0].split(', '))
+                rng_vals = [s.strip(' ') for s in range_tokens[0].split(',')]
                 rng_vals = [int(s) for s in rng_vals]
                 range_tokens = [rng_vals, range_tokens[1]]
                 ranges.append(range_tokens)
