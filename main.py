8d7103a988305c26967e110f29093df5d2114bff
GhastCMD <kraken32bin@protonmail.com>
2021-06-01 00:54:04 -0300

    The program now can parse question database file

--- main.py
+++ main.py
@@ -31,16 +31,61 @@ Para verificar algum alvo arbitrário, basta chamar a função
 predict da árvore e colocar os valores arbitrários como parâmetros.
 '''
 
-def read_file(file_path: str):
-    values = []
+class Disturbance:
+    
+    def __init__(self, name, questions, values, ranges):
+        self.name = name
+        self.questions = questions[:]
+        self.values = values[:]
+        self.ranges = ranges[:]
+
+    def print(self):
+        print(self.name)
+        for quest in self.questions:
+            print(quest)
+        for value in self.values:
+            print(value)
+        for range in self.ranges:
+            print(range)
+
+def parse_file(file_path: str):
+    dists = []
     with open(file_path) as fp:
-        for 
-        line = fp.readline()
-        if line[0] == '#':
-            line = line[1:].strip()
-        values.append(line)
-    return values
+        name = ''
+        questions = []
+        values = []
+        ranges = []
+
+        for line in fp:
+            line = line.strip()
+            if line == '*':
+                dist = Disturbance(name, questions, values, ranges)
+                dists.append(dist)
+                name = ''
+                questions.clear()
+                values.clear()
+                ranges.clear()
+            elif line[0] == '#':
+                name = line[1:].strip()
+            elif line[0] == '<':
+                line.index('(')
+                value_tokens = line.strip('<> ').split(', ')
+                value_tokens = [s.strip(')').split('(') for s in value_tokens]
+                value_tokens = [[s[0], int(s[1])] for s in value_tokens]
+                values.append(value_tokens)
+            elif line[0] == '[':
+                range_tokens = line.strip('[ ').split('] ')
+                # range_tokens = [s.split(', ') for s in range_tokens]
+                rng_vals = (range_tokens[0].split(', '))
+                rng_vals = [int(s) for s in rng_vals]
+                range_tokens = [rng_vals, range_tokens[1]]
+                ranges.append(range_tokens)
+            else:
+                questions.append(line)
+
+    return dists
 
 if __name__ == '__main__':
-    values = read_file('question-db.dat')
-    print(values)
+    folder = parse_file('question-db.dat')
+    for attr in folder:
+        attr.print()
