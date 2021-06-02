aa669c49411613c1cac3551636db52714c8633d9
GhastCMD <kraken32bin@protonmail.com>
2021-06-01 22:08:52 -0300

    Corrected a bug with user input wrongly processes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from unidecode import unidecode
+import sys
+
+debug = False
 
 def comp_str(string: str):
     return unidecode(string.lower())
@@ -33,25 +36,28 @@ class Disturbance:
         param_question = param_question.strip(', ')
         param_question += '>\n'
         
-        low_param_question = unidecode(param_question.lower())
+        low_param_list = [unidecode(s[0].lower()) for s in self.values]
 
         for question in self.questions:
             print(question)
-            print(param_question)
-            answer = '2'
+            global debug
+            if not debug:
+                answer = input(param_question)
+            else:
+                answer = '2'
             answer_not_good = True
+            answer = answer.strip(' \r\n')
             while answer_not_good:
                 if answer in '< , - >':
                     answer = 'none'
                 if answer in number_values:
                     answer = self.values[int(answer)][0]
                 answer = unidecode(answer.lower())
-                if answer in low_param_question:
+                if answer in low_param_list:
                     answer_not_good = False
                 else:
                     answer = input('Reposta inválida, tente novamente\n')
-            index = [unidecode(s[0].lower()) for s in self.values]
-            index = index.index(answer)
+            index = low_param_list.index(answer)
             self.score += self.values[index][1]
     
     # This method fits the score gotten into the ranges from the db
@@ -60,8 +66,6 @@ class Disturbance:
             if self.score >= first and self.score <= second:
                 self.disturb_status = word
                 self.status_index = i
-                # for index in range(len(self.recomend) -1, i, -1):
-                #     del self.recomend[index]
                 break
 
     def routine(self):
@@ -69,7 +73,7 @@ class Disturbance:
         self.fit_param()
     
     def print_status(self):
-        print(f'{self.name}:\n\t{self.disturb_status}')
+        print(f'{self.name}: {self.disturb_status}')
 
     def print_recomendation(self):
         for i in self.recomend[self.status_index:-1]:
@@ -136,12 +140,16 @@ class FileParser:
         return self.dists
 
 if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == 'debug':
+            debug = True
+
     folder = FileParser().parse_file('question-db.dat')
 
     for attr in folder:
         attr.routine()
     
-    print('O seu diagnóstico é:')
+    print('O diagnóstico é:')
     
     for attr in folder:
         attr.print_status()
