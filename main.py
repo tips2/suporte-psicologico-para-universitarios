e319a868f38f020bff2679960c83636e58c0c3a8
GhastCMD <kraken32bin@protonmail.com>
2021-06-03 00:23:18 -0300

    Added a introductory message to the program

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
 
     folder = FileParser(debug).parse_file('question-db.dat')
 
+    print('\nO sistema especialista realiza o diagnóstico do Transtorno de Ansiedade e Transtorno Depressivo. O intúito é dar recomendações básicas a cerca dos diagnósticos obtidos para que o usuário possa realizar as primeiras providências para sanar os problemas.\n\nO usuário deverá responder as perguntas com os valores que estão entre parênteses, sendo eles em texto natural, como "sim" ou "Não", como também no valor da posição da resposta, como no caso do "não", que está na posição 0, logo, quando o usuário digitar 0 como resposta estará respondendo também com o valor "Não".\n*Note que o programa não sabe diferenciar entre minúsculas, maiúsculas e letras com acentos*\n\n')
+
     for attr in folder:
         attr.routine()
         
@@ -24,6 +26,6 @@ if __name__ == '__main__':
         attr.print_recomendation()
     
     if folder[0].score >= 1 and folder[1].score >= 1:
-        print('\nPara ansiedade e Depressão:\nServiço de Acolhimento Psicológico, Proest:\nConsiste no acolhimento de estudantes por psicólogos da Pró-reitoria Estudantil Serviço de Acolhimento Psicológico\nO que é?\nAcolhimento de estudantes por psicólogos da Pró-reitoria Estudantil (Proest) para orientação ou possível encaminhamento para rede SUS\nacesse: https://servicos.ufal.br/orgaos/pro-reitoria-estudantil-proest/servico-de-acolhimento-psicologico\n\n')
+        print('\nPara ansiedade e Depressão:\nServiço de Acolhimento Psicológico, Proest:\nConsiste no acolhimento de estudantes por psicólogos da Pró-reitoria Estudantil Serviço de Acolhimento Psicológico\nO que é ?\nAcolhimento de estudantes por psicólogos da Pró-reitoria Estudantil (Proest) para orientação ou possível encaminhamento para rede SUS\nacesse: https://servicos.ufal.br/orgaos/pro-reitoria-estudantil-proest/servico-de-acolhimento-psicologico\n\n')
     
     print('Lembre-se que essas soluções não substituem o tratamento real, procure sempre ajuda!\n')
